dadfb7bcb6383147cb82b76b0e32fee4f1129594
Gnanavel <gnanaveln@gmail.com>
2016-10-22 00:04:45 +0530

    US001 - Angular Routing

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,7 +1,28 @@
-angular.module('SMApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
+angular.module('SMApp', 
+	['ui.router',
+	 'ngAnimate',
+	 'ngSanitize',
+	 'ui.bootstrap'])
+
+.config(function($stateProvider) {
+  var cityState = {
+    name: 'cityMaster',
+    url: '/cityMaster',
+    template: '<h3>City Master</h3>'
+  };
+  
+  var branchState = {
+    name: 'branchMaster',
+    url: '/branchMaster',
+    template: '<h3>Branch Master</h3>'
+  };
+  
+  $stateProvider.state(cityState);
+  $stateProvider.state(branchState);
+})
 
 .controller('CollapseMenuCtrl',['$scope', '$log', function($scope,$log) {
-	$scope.isTransCollapsed = true;
+	/*$scope.isTransCollapsed = true;
 	$scope.isMasterCollapsed = true;
-	$scope.isReportCollapsed = true;
+	$scope.isReportCollapsed = true;*/
 }]);
